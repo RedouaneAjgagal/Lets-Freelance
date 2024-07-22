159980eb48b7b4e84fdb37095b58feb7b8654b6a
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-07-22 09:32:35 +0100

    [FIX] advertisement links not showing on hover sometimes for mobile

--- client/src/features/advertisement/components/AdvertisementNavbar.tsx
+++ client/src/features/advertisement/components/AdvertisementNavbar.tsx
@@ -9,24 +9,24 @@ const AdvertisementNavbar = () => {
                 <div className="group relative z-10">
                     <button className="flex items-center gap-1 py-1">
                         Payment methods
-                        <TbArrowDown size={18} className="transition-all duration-500 group-hover:rotate-180" />
+                        <TbArrowDown size={18} className="transition-all duration-500 group-focus-within:rotate-180" />
                     </button>
-                    <div className="transition-all duration-500 opacity-0 -translate-y-4 invisible group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 pt-1 absolute left-0 top-8">
+                    <div className="transition-all duration-500 opacity-0 -translate-y-4 invisible group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-0 absolute left-0 top-8 min-w-[17rem]">
                         <ul className=" bg-white p-3 min-w-[15rem] border rounded shadow-lg text-base">
-                            <li className="flex"><Link to={`/profile/freelancer/advertisements/payment-methods`} className="border-b py-2 w-full">My payment methods</Link></li>
-                            <li className="flex"><Link to={`/profile/freelancer/advertisements/payment-methods/create`} className="py-2 w-full">Add a payment method</Link></li>
+                            <li className="flex"><Link to={`/profile/freelancer/advertisements/payment-methods`} className="border-b p-2 w-full hover:bg-slate-100 hover:rounded">My payment methods</Link></li>
+                            <li className="flex"><Link to={`/profile/freelancer/advertisements/payment-methods/create`} className="p-2 w-full hover:bg-slate-100 hover:rounded">Add a payment method</Link></li>
                         </ul>
                     </div>
                 </div>
                 <div className="group relative z-10">
                     <button className="flex items-center gap-1 py-1">
                         Campaigns
-                        <TbArrowDown size={18} className="transition-all duration-500 group-hover:rotate-180" />
+                        <TbArrowDown size={18} className="transition-all duration-500 group-focus-within:rotate-180" />
                     </button>
-                    <div className="transition-all duration-500 opacity-0 -translate-y-4 invisible group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 pt-1 absolute right-0 top-8">
+                    <div className="transition-all duration-500 opacity-0 -translate-y-4 invisible group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-0 absolute right-0 top-8 cursor-pointer min-w-[17rem]">
                         <ul className=" bg-white p-3 min-w-[15rem] border rounded shadow-lg text-base">
-                            <li className="flex"><Link to={`/profile/freelancer/advertisements/campaigns`} className="border-b py-2 w-full">My Campaigns</Link></li>
-                            <li className="flex"><Link to={`/profile/freelancer/advertisements/create/campaign`} className="py-2 w-full">Create new campaign</Link></li>
+                            <li className="flex"><Link to={`/profile/freelancer/advertisements/campaigns`} className="border-b p-2 w-full hover:bg-slate-100 hover:rounded">My Campaigns</Link></li>
+                            <li className="flex"><Link to={`/profile/freelancer/advertisements/create/campaign`} className="p-2 w-full hover:bg-slate-100 hover:rounded">Create new campaign</Link></li>
                         </ul>
                     </div>
                 </div>

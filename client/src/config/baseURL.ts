eb719d7d6f890d02b78e667747e41ce3c2baf118
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-09-01 19:56:37 +0100

    [chore] set new active production server url

--- client/src/config/baseURL.ts
+++ client/src/config/baseURL.ts
@@ -2,8 +2,8 @@
 // export const noApiBaseUrl = "localhost:5000"
 // export const clientURL = "http://localhost:5173";
 
-const baseURL = "https://lets-freelance-49rx.onrender.com/api/v1";
-export const noApiBaseUrl = "lets-freelance-49rx.onrender.com"
+const baseURL = "https://lets-freelance.onrender.com/api/v1";
+export const noApiBaseUrl = "lets-freelance.onrender.com";
 export const clientURL = "https://lets-freelance.netlify.app";
 
 export default baseURL;
--- server/config/origin.ts
+++ server/config/origin.ts
@@ -1,6 +1,6 @@
 // const origin = "http://localhost:5173"
 const origin = "https://lets-freelance.netlify.app"
 
-export const backendUrl = "https://lets-freelance-49rx.onrender.com";
+export const backendUrl = "https://lets-freelance.onrender.com";
 
 export default origin;
